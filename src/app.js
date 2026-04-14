653c995dc3fb65859f81c88c4890e52d3d8f43c5
Lucas MacKenzie <lucas.mackenzie@gmail.com>
2026-04-14 12:14:28 -0300

    Added child page to router and toast container to app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
+// import "@awesome.me/webawesome/dist/styles/webawesome.css";
+/* Theme */
+import "@web.awesome.me/webawesome-pro/dist/styles/themes/default.css";
+/* Native styles */
+import "@web.awesome.me/webawesome-pro/dist/styles/native.css";
+/* CSS utilities */
+import "@web.awesome.me/webawesome-pro/dist/styles/utilities.css";
+
+
 import "@shoelace-style/shoelace/dist/themes/light.css";
 import "@firebase-oss/ui-styles/dist.min.css";
 import {setBasePath} from "@shoelace-style/shoelace/dist/utilities/base-path.js";
@@ -18,71 +27,85 @@ import AddPage from "./components/AddPage";
 import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
 import ProfilePage from "./components/ProfilePage";
+import ChildPage from "./components/ChildPage";
 import {signOut} from "firebase/auth";
 import {auth} from "./firebase";
+
+import WaToast from "@web.awesome.me/webawesome-pro/dist/react/toast";
 
 const handleLogout = () => signOut(auth);
 
 const App = () => {
-	return (
-		<FirebaseUIProvider ui={ui}>
-			<AuthProvider>
-				<Router>
-					<nav
-						className="menu"
-						style={{display: "none"}}
-					>
-						<Link to="/">Index</Link>
-						<Link to="/edit">Edit</Link>
-						<Link to="/add">Add</Link>
-						<Link to="/profile">Profile</Link>
-						<button onClick={handleLogout}>Logout</button>
-					</nav>
-					<Routes>
-						<Route
-							path="/"
-							element={
-								<ProtectedRoute>
-									<Timeline />
-								</ProtectedRoute>
-							}
-						/>
-						<Route
-							path="/edit/:entryId"
-							element={
-								<ProtectedRoute>
-									<EditPage />
-								</ProtectedRoute>
-							}
-						/>
-						<Route
-							path="/add"
-							element={
-								<ProtectedRoute>
-									<AddPage />
-								</ProtectedRoute>
-							}
-						/>
-						<Route
-							path="/profile"
-							element={
-								<ProtectedRoute>
-									<ProfilePage />
-								</ProtectedRoute>
-							}
-						/>
-						<Route
-							path="/login"
-							element={<LoginPage />}
-						/>
-						<Route
-							path="/signup"
-							element={<SignupPage />}
-						/>
-					</Routes>
-				</Router>
-			</AuthProvider>
-		</FirebaseUIProvider>
+    return (
+		<div>
+			<WaToast></WaToast>
+			<FirebaseUIProvider ui={ui}>
+				<AuthProvider>
+					<Router>
+						<nav
+							className="menu"
+							style={{display: "none"}}
+						>
+							<Link to="/">Index</Link>
+							<Link to="/edit">Edit</Link>
+							<Link to="/add">Add</Link>
+							<Link to="/profile">Profile</Link>
+							<button onClick={handleLogout}>Logout</button>
+						</nav>
+						<Routes>
+							<Route
+								path="/"
+								element={
+									<ProtectedRoute>
+										<Timeline />
+									</ProtectedRoute>
+								}
+							/>
+							<Route
+								path="/edit/:entryId"
+								element={
+									<ProtectedRoute>
+										<EditPage />
+									</ProtectedRoute>
+								}
+							/>
+							<Route
+								path="/add"
+								element={
+									<ProtectedRoute>
+										<AddPage />
+									</ProtectedRoute>
+								}
+							/>
+							<Route
+								path="/profile"
+								element={
+									<ProtectedRoute>
+										<ProfilePage />
+									</ProtectedRoute>
+								}
+							/>
+							<Route
+								path="/child/:childId?"
+								element={
+									<ProtectedRoute>
+										<ChildPage />
+									</ProtectedRoute>
+								}
+							/>
+							<Route
+								path="/login"
+								element={<LoginPage />}
+							/>
+							<Route
+								path="/signup"
+								element={<SignupPage />}
+							/>
+						</Routes>
+					</Router>
+				</AuthProvider>
+			</FirebaseUIProvider>
+		</div>
 	);
 };
 
